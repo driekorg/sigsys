c250a3189e98b81f52a231c5141bd2a02263f3d8
ruurd <ciw8053@wing.rug.nl>
2010-02-18 16:34:37 +0100

    Text verslag aangepast en plaatjes beter gemaakt

--- matlab/TestDiff.m
+++ matlab/TestDiff.m
@@ -1,6 +1,6 @@
 function [] = TestDiff()
 %TESTDIFF Plot DF_Gen theta vs our own calculated theta
-
+	%901 samples are taken between -400 and 500 where our own calculated theta via the function CalcDir is compared to the theta from DF_Gen via a plot.
     x = -400:500;
     [X1, X2, originalTheta] = DF_gen(x);
     ourTheta = CalcDir(X1, X2);
