466e965bedfe7e3769aa1bdd241ccd7e0036291f
ruurd <ciw8053@wing.rug.nl>
2010-02-24 12:51:36 +0100

    Verslag project 2 bijna af

--- p2/functions/playfm.m
+++ p2/functions/playfm.m
@@ -1,13 +1,12 @@
 function xx = playfm( fc, fm, fic, fim, A0, I0, tau, dur, fsamp )
-%PLAYFM Summary of this function goes here
-%   Detailed explanation goes here
+%function calculates fm  function with given values. After which the created sound is played.
 
 [tt, yy] = bellenv(tau, dur, fsamp);
 At = A0 * yy;
 It = I0 * yy;
 
 xx = At .* cos((2*pi*fc*tt) + fic + (It .* cos(2*pi*fm*tt + fim)));
-%soundsc(xx, fsamp);
+soundsc(xx, fsamp);
 
 end
 
--- p2/functions/spectro.m
+++ p2/functions/spectro.m
@@ -1,8 +1,8 @@
 function spectro( xx )
+%function creates clear spectogram for an input with a sampling rate of 11025
 
 [S,F,T] = spectrogram(xx, 1102, 1000, 1024, 11025);
 imagesc(T, F, abs(S))
 axis xy
 
 end
-
